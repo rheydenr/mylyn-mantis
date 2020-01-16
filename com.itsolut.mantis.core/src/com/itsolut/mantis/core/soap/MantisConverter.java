dbe66bbf8436434357458f8284d47b1e26fbbddd
Ralf Heydenreich <ralf.heydenreich@itemis.de>
2020-01-16 20:28:49 +0100

    Implement REST client
    initial implementation

--- com.itsolut.mantis.core/src/com/itsolut/mantis/core/soap/MantisConverter.java
+++ com.itsolut.mantis.core/src/com/itsolut/mantis/core/soap/MantisConverter.java
@@ -54,7 +54,7 @@ public class MantisConverter {
         return version;
     }
 
-    public static MantisTicket convert(IssueData issue, MantisSoapClient mantisClient, IProgressMonitor monitor) throws MantisException {
+    public static MantisTicket convert(IssueData issue, IMantisClient mantisClient, IProgressMonitor monitor) throws MantisException {
 
         MantisTicket ticket = new MantisTicket(issue.getId().intValue());
         ticket.setCreated(issue.getDate_submitted().getTime());
